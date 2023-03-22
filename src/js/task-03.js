36271645e33d2bcbe079c4f54baf936fb9b7d640
oleniuh <maxim.oleniukh@gmail.com>
2023-03-22 14:35:49 +0000

    styles for task-03
    display flex and list style none

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,38 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery') // select ul gallery 
+
+// function to create a card
+
+const makeGalleryCard = ({ width, height, url, alt } = {}) => {
+  return `
+    <li>
+      <a href="#">
+        <img src="${url}" alt="${alt}" width="${width}" height="${height}">
+      </a>
+    </li>
+  `;
+};
+// creating an array with cards 
+const galleryCardsArray = images.map(image => {
+  return makeGalleryCard(image);
+});
+// console.log(galleryCardsArray);
+const result = gallery.insertAdjacentHTML('beforeend', galleryCardsArray.join(''));
+
+/*
+<li>
+ <a href="#">
+    <img url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',>
+    </a>
+</li>
+*/
+
+/* Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. 
+Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.*/
