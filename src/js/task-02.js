59d1aa610d950ec90821637918b4134fa420520c
oleniuh <maxim.oleniukh@gmail.com>
2023-03-22 13:21:40 +0000

    update task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsUl = document.querySelector('ul#ingredients');
+
+const result = ingredients.map((ingredient) => { // create new ul of list. Cycle repeats for each li.
+  const li = document.createElement('li'); // create <li></li>
+  li.textContent = ingredient; // <li>ingredient</li>
+  li.classList.add('item'); // add class <li class="item">ingredient</li>
+  return li; // return <li>ingredient</li>
+});
+
+ingredientsUl.append(...result); // add each li to an empty ul
+
+
+
+
+
+
+// HTML містить порожній список ul#ingredients.
+
+// ? Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.  
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+// */
