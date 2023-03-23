5bb8c77cd3f354fd14c8dc14ad98c1c38a7ff1a5
oleniuh <maxim.oleniukh@gmail.com>
2023-03-23 19:21:01 +0000

    update task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,23 @@
-function getRandomHexColor() {
+const btn = document.querySelector('.change-color')
+const colorValue = document.querySelector('.color');
+
+function getRandomHexColor() { 
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const backgroundColorHandler = (e => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
+
+btn.addEventListener('click', backgroundColorHandler)
+
+/*
+
+Напиши скрипт, який змінює кольори фону елемента <body> 
+через інлайн-стиль по кліку на button.change-color 
+і виводить значення кольору в span.color.
+*/
