edc6f4fbe3ddf1d4421109cff443f9ef911b016c
oleniuh <maxim.oleniukh@gmail.com>
2023-03-23 19:24:04 +0000

    task-09 finished

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,19 +1,19 @@
-const btn = document.querySelector('.change-color')
+const btn = document.querySelector('.change-color');
 const colorValue = document.querySelector('.color');
 
 function getRandomHexColor() { 
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
-const backgroundColorHandler = (e => {
+const backgroundColorHandler = () => {
   const randomColor = getRandomHexColor();
   document.body.style.backgroundColor = randomColor;
   colorValue.textContent = randomColor;
-});
+};
 
-btn.addEventListener('click', backgroundColorHandler)
+btn.addEventListener('click', backgroundColorHandler);
 
 /*
 
